b8cac99758e18b0f84477ed627e7f8b1d500dd90
Sam Cameron <scameron@cars.com>
2019-06-25 11:25:05 -0700

    Problem 2 solution

--- problems/problem2/src/main.rs
+++ problems/problem2/src/main.rs
@@ -9,13 +9,14 @@ fn main() {
         // this variable can be used to store the string input from the user
         let mut number = String::new();
 
-        // this should be replaced with code that tries to parse `number`
-        // into a value of `u32`, or otherwise go back to the beginning of the
-        // loop
-        let number: u32 = 0;
+        io::stdin().read_line(&mut number)
+            .expect("Failed to read number");
 
-        // make sure to modify `sum_one_to_n` in `libr.rs`. the stub
-        // implementation included with this problem always returns 0.
+        let number: u32 = match number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         let result = sum_one_to_n(number);
 
         println!("The sum of 1 to {} is: {}", number, result);
